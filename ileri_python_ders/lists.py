290b78e0690b0cf967e5d05edd4e28be62931b01
zafer galip ozberk <zaferozberk@gmail.com>
2022-01-22 00:01:16 +0300

    listeler

---
+++ ileri_python_ders/lists.py
@@ -0,0 +1,107 @@
+# Lists: ordered, mutable, allows duplicate elements
+from subprocess import list2cmdline
+
+
+def listele(liste):
+    say  = len(liste)
+    x=1
+    for item in liste:
+        if x == say:
+            print(item,end="\n")
+        else:
+            print(item,end=",")
+            x += 1    
+
+mylist = ["a","b","c","d","e","f"]
+
+print('list')
+listele(mylist)
+print("-"*20)
+
+print('clear()')
+mylist.clear()
+listele(mylist)
+print("-"*20)
+
+mylist = ["a","b","c","d","e","f"]
+
+print('append("g")')
+mylist.append("g")
+listele(mylist)
+print("-"*20)
+
+print('insert(3,"h")')
+mylist.insert(3,"h")
+listele(mylist)
+print("-"*20)
+
+print('pop()')
+mylist.pop()
+listele(mylist)
+print("-"*20)
+
+print('remove("e")')
+mylist.remove("e")
+listele(mylist)
+print("-"*20)
+
+print('reverse()')
+mylist.reverse()
+listele(mylist)
+print("-"*20)
+
+print('sorted(mylist')
+new_list = sorted(mylist)
+listele(new_list)
+listele(mylist)
+print("-"*20)
+
+print('sort()')
+mylist.sort()
+listele(mylist)
+print("-"*20)
+
+print('list =[0] * 5')
+mylist1 = [0] * 5
+listele(mylist1)
+print("-"*20)
+
+print('new_list = mylist + mylist2')
+mylist2 = [1,2,3,4,5,6,7]
+new_list = mylist + mylist2
+listele(new_list)
+print("-"*20)
+
+a = [1,2,3,4,5,6,7,8,9]
+
+print('b = a[1:5]')
+b = a[1:5]
+listele(b)
+print("-"*20)
+
+print('b = a[::1]')
+b = a[::1]
+listele(b)
+print("-"*20)
+
+print('b = a[::-1]')
+b = a[::-1]
+listele(b)
+print("-"*20)
+
+print("list_cpy = list_org.copy()")
+print('list_cpy.append("d")')
+list_org = ["a","b","c"]
+list_cpy = list_org.copy()
+list_cpy.append("d")
+listele(list_org)
+listele(list_cpy)
+print("-"*20)
+
+mylist = [1,2,3,4,5,6]
+print("mylist2 = [i** for i in mylist]")
+b = [item*item for item in mylist]
+listele(mylist)
+listele(b)
+print("-"*20)
+
