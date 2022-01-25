00fda6394a2c45d86b18d57804a8fdb0735cdd23
zafer galip ozberk <zaferozberk@gmail.com>
2022-01-25 14:16:32 +0300

    mongo

--- mongodb_test/mongo.py
+++ mongodb_test/mongo.py
@@ -1,9 +1,11 @@
 from os import sep
 from mongoengine import Document, StringField
 
+
 class Todos(Document):
     title = StringField(required=True, max_length=70)
     author = StringField(required=True, max_length=20)
+
 
 def baglan():
     from mongoengine import connect
@@ -12,23 +14,24 @@ def baglan():
     print(client)
     print("*"*20)
 
+
 def ekle():
     t1 = Todos(
         title="Beautiful Soup: Build a Web Scraper With Python",
         author="Martin"
-        )
+    )
 
-    t1.save() 
+    t1.save()
     print("Kayıt Eklendi.")
     print("*"*20)
 
+
 def listele():
     for doc in Todos.objects:
-        print("Title : ",doc.title,end="   *X*   ")
-        print("Author : ",doc.author,end="\n")
+        print("Title : ", doc.title, end="   *X*   ")
+        print("Author : ", doc.author, end="\n")
+
 
 baglan()
 # ekle()
 listele()
-
-
