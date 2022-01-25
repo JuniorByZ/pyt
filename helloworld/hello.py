de04fe9c3a63a75a403178274fba3d2aa83b37c6
zafer galip ozberk <zaferozberk@gmail.com>
2022-01-25 17:19:08 +0300

    silinen

--- helloworld/hello.py
+++
@@ -1,6 +0,0 @@
-def main(name):
-    print(f"Hello {name}")
-
-if __name__ == "__main__":
-    main("Zafer")
-
--- teldefteri/baglanti.py
+++
@@ -1,2 +0,0 @@
-global baglan
-baglan = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
